5583b0ea3fea23b7baf16a1a33134deda87b4b06
Meena E <meenarekhadvelmrm@gmail.com>
2025-05-05 14:17:19 +0530

    app2.py

--- app2.py
+++ app2.py
@@ -95,4 +95,4 @@ def predict():
 
 # Start the app
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
